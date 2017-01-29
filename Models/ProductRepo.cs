7996d700214838d64dca0fbd24b994c0576d2a09
Julius Alvarado <angularjs.uiux@gmail.com>
2017-01-28 22:34:12 -0800

    asp.net web api project
    3rd repo of a web api project I'm working on, I broke the other 2 I was
    working on and couldn't figure out how to fix it so hopefully this one
    doesn't break.

---
+++ Models/ProductRepo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json; 
+using System.Web.Hosting; 
+
+namespace WebApp1.Models
+{
+    /// <summary>
+    /// This class will store data in a local json file.
+    /// </summary>
+    public class ProductRepo
+    {
+        /// <summary>
+        /// This function will create a new Product w/default vals.  
+        /// </summary>
+        /// <returns></returns>
+        internal Product Create()
+        {
+            var product = new Product { ReleaseDate = DateTime.Now };
+            return product; 
+        }
+
+        internal List<Product> Retrieve()
+        {
+            var filePath = HostingEnvironment.MapPath(@"~/App_Data/product.json");
+            var json = System.IO.File.ReadAllText(filePath);
+            var products = JsonConvert.DeserializeObject<List<Product>>(json);
+            return products; 
+        }
+    }
+}
