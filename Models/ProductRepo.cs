8d337117bcf14cd6b549a63c2695ce11ad07e37d
Julius Alvarado <angularjs.uiux@gmail.com>
2017-01-30 21:45:59 -0800

    added post/put and error handling
    implemented post and put actions in ctrl, and added error handling and
    backend validation and sent back action results .

--- Models/ProductRepo.cs
+++ Models/ProductRepo.cs
@@ -12,21 +12,68 @@ namespace WebApp1.Models
     public class ProductRepo
     {
         /// <summary>
-        /// This function will create a new Product w/default vals.  
+        /// Creates a new Product w/default vals.  
         /// </summary>
         /// <returns></returns>
-        internal Product Create()
+        internal ProductModel Create()
         {
-            var product = new Product { ReleaseDate = DateTime.Now };
+            var product = new ProductModel { ReleaseDate = DateTime.Now };
             return product; 
         }
 
-        internal List<Product> Retrieve()
+        /// <summary>
+        /// Returns all the data from the local json file.  
+        /// </summary>
+        /// <returns></returns>
+        internal List<ProductModel> Retrieve()
         {
             var filePath = HostingEnvironment.MapPath(@"~/App_Data/product.json");
             var json = System.IO.File.ReadAllText(filePath);
-            var products = JsonConvert.DeserializeObject<List<Product>>(json);
+            var products = JsonConvert.DeserializeObject<List<ProductModel>>(json);
             return products; 
+        }
+
+        /// <summary>
+        /// POSTS a product to the local json file.  
+        /// </summary>
+        /// <params name="product"></params>
+        /// <returns></returns>
+        internal ProductModel Save(ProductModel product)
+        {
+            var products = this.Retrieve();
+            var maxId = products.Max(p => p.ProductId);
+            product.ProductId = maxId + 1;
+            products.Add(product);
+            WriteData(products);
+            return product; 
+        }
+
+        /// <summary>
+        /// PUTS a product to the local json file.  
+        /// </summary>
+        /// <params name="product"></params>
+        /// <params name="productId"></params>
+        /// <returns></returns>
+        internal ProductModel Save(ProductModel product, int productId)
+        {
+            var products = this.Retrieve();
+            var itemIndex = products.FindIndex(p => p.ProductId == product.ProductId);
+            if (itemIndex > 0) products[itemIndex] = product;
+            else return null;
+
+            WriteData(products);
+            return product; 
+        }
+
+        private bool WriteData(List<ProductModel> products)
+        {
+            // write out the json
+            var filePath = HostingEnvironment.MapPath(@"~/App_Data/product.json");
+
+            var json = JsonConvert.SerializeObject(products, Formatting.Indented);
+            System.IO.File.WriteAllText(filePath, json);
+
+            return true; 
         }
     }
 }
